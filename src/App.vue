b5f8b302083c6d8c082feac72c52c13e8cee0bea
Chris Gartsen <chris@gartsen.nl>
2018-04-20 17:21:20 +0200

    Clean up app file

--- src/App.vue
+++ src/App.vue
@@ -18,21 +18,6 @@
 
 <script>
 export default {
-  data () {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
-  },
   name: 'App'
 }
 </script>
