c570ded013ec1977c268efff89d0b1ec02925f5b
Chris Gartsen <chris@gartsen.nl>
2018-04-20 16:59:44 +0200

    Clean up routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import main from '@/components/main-page'
-import signin from '@/components/auth/signin-page'
-import signup from '@/components/auth/signup-page'
-import groceries from '@/components/stock/groceries-page'
-import items from '@/components/stock/items-page'
+
+import home       from '@/components/home-page'
+import signin     from '@/components/auth/signin-page'
+import signup     from '@/components/auth/signup-page'
+import groceries  from '@/components/stock/groceries-page'
+import items      from '@/components/stock/items-page'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/',          name: 'main',      component: main },
+    { path: '/',          name: 'home',      component: home },
     { path: '/signin',    name: 'signIn',    component: signin },
     { path: '/signup',    name: 'signUp',    component: signup },
     { path: '/groceries', name: 'groceries', component: groceries },
