fa25b5fedcefb5976e73c2016285d68f2e33aa72
Chris Gartsen <github@gartsen.nl>
2018-04-21 23:36:20 +0200

    Add Vuex support

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import store from './store'
 
 // Vuetify
 import Vuetify from 'vuetify'
@@ -23,6 +24,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  store,
   components: { App },
   template: '<App/>'
 })
